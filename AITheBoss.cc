769f36504667fb74004bb0f073af66d8896e1175
Daniel <onishimasradio@gmail.com>
2016-11-24 21:10:12 +0100

    bfs

--- AITheBoss.cc
+++ AITheBoss.cc
@@ -21,42 +21,104 @@ struct PLAYER_NAME : public Player {
   typedef vector<int> VE;
   typedef vector <vector <bool> > graph;
 
-  bool bfs(graph& g, int x, int y){
-    if (!g[x][y]) {
-      g[x][y] = true;
+  map <int, bool> path; //farmers map para saber si tienen camino si no tienen bfs
 
+
+
+  bool security(const Pos& p) {
+    if (cell(p).type == Wall) return false;
+    else if (cell(p).haunted) return false;
+    else if (cell(p).id != -1) return false;
+    else return true;
+  }
+
+  Dir dirfast(Pos& act, Pos& aux) {
+    int x,y,a,b;
+    x = act.i;
+    y = act.j;
+    a = aux.i;
+    b = aux.j;
+    if ( x == a and y < b) return Top;
+    if ( x == a and y > b) return Bottom;
+    if ( x > a and y == b) return Left;
+    if ( x < a and y == b) return Right;
+    else None;
+  }
+
+  // TODO: si hay mas de una posicion vacia random!!!!
+  Pos bfs_farmers(graph& g, Pos& pos, queue <Pos>& Q ){
+    bool found = false; // camino
+    Q.push(pos);
+    while (not Q.empty() and not found) {
+      Pos aux = Q.front();  Q.pop();
+      int x = aux.i;
+      int y = aux.j;
+      if (!g[x][y]) {
+        g[x][y] = true;
+        Pos aux2;
+       aux2 = aux + Top;
+        if ( security(aux2) and cell(aux2).owner >= 0) {
+          Q.push(aux2);
+        }
+        else if (security(aux2) and cell(aux2).owner == -1) {
+          found = true;
+          return aux2;
+        }
+        aux2 = aux + Right;
+        if ( security(aux2) and cell(aux2).owner >= 0) {
+          Q.push(aux2);
+        }
+        else if (security(aux2) and cell(aux2).owner == -1) {
+          found = true;
+          return aux2;
+        }
+        aux2 = aux + Left;
+        if ( security(aux2) and cell(aux2).owner >= 0) {
+           Q.push(aux2);
+        }
+        else if (security(aux2) and cell(aux2).owner == -1) {
+          found = true;
+          return aux2;
+        }
+        aux2 = aux + Bottom;
+        if ( security(aux2) and cell(aux2).owner >= 0) {
+          Q.push(aux2);
+        }
+        else if (security(aux2) and cell(aux2).owner == -1) {
+          found = true;
+          return aux2;
+        }
+      }
     }
   }
 
 
   virtual void play () {
-    //TODO farmers dfs teniendo en cuenta que no este ocupada
-    // que no haya brujan a distancia manhattan y knights. Si mal salud tirar para atras
     VE f = farmers(0);
     for (int id : f) {
       graph g(37, vector<bool>(37, false));
-      if (unit(id).health >= 50) {
-        int x,y;
-        x = unit(id).pos.i;
-        y = unit(id).pos.j;
-        //TODO bfs de hasta distancia tres y si todos libres random
-
-      }
-      else command(id, None);
+    //  map <int, bool>::const_iterator aux = path.find(id);
+    //  if (aux == path.end() ) path.insert(make_pair(id, false));
+      //  map <int, bool>::const_iterator i = path.find(id);
+    //  if (unit(id).health >= 50 and (i->second == false)) {
+        Pos act = unit(id).pos;
+        Pos aux;
+        queue <Pos> Q;
+        aux = bfs_farmers(g, act, Q);
+        command(id, dirfast(act,aux) );
+      //}
     }
 
 
     VE k = knights(0);
     for (int id : k) {
       //TODO: knight
-      command(id, Left);
 
     }
     VE w = witches(0);
     for (int id : w) {
-
-    }      //TODO: bruja bfs que mate todo aquello que este cerca
-      command(id, None);
+        //TODO: bruja bfs que mate todo aquello que este cerca
+    }
   }
 };
 
